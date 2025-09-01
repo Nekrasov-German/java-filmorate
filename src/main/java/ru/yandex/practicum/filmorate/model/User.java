1af42e4ae79dfc62e7bbb53594b650bb71dcd86a
German Nekrasov <gazelgerman@yandex.ru>
2025-09-01 13:01:47 +0500

    Добавить аннотацию PositiveDuration. Добавить проверку на дату релиза при обновлении. Добавить проверку на пробелы в поле login.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -15,6 +12,7 @@ public class User {
     private String email;
     @NotNull(message = "Login не может быть пустым")
     @NotBlank(message = "Login не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
     private String name;
     @NotNull(message = "Дата рождения не может быть пустой")
