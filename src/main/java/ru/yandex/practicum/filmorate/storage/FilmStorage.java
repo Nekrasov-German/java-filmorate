8fa2b45a5f2336201171cc79a1418a96beaddb71
German Nekrasov <gazelgerman@yandex.ru>
2025-09-12 15:43:59 +0500

    Добавить новую функциональность добавления в друзья. Добавить Like для фильмов. Перестроить архитектуру по разделению логики service, component, controller.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 public interface FilmStorage {
+
     Collection<Film> findAll();
+
     Film create(Film film);
+
     Film update(Film film);
+
     void delete(Film film);
+
     Film findById(Long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
 
 public interface UserStorage {
+
     Collection<User> findAll();
+
     User create(User user);
+
     User update(User user);
+
     void delete(User user);
+
     User findById(Long id);
 }
